788c913e5b4f6b3fa56f15f70a9156dceda6b17c
IsYet1 <Don@IsYet.com>
2021-11-08 15:02:51 -0600

    Add manager and save function

---
+++ GroceryApp/Models/Store.swift
@@ -0,0 +1,14 @@
+//
+//  Store.swift
+//  GroceryApp
+//
+//  Created by Don McKenzie on 11/8/21.
+//
+
+import Foundation
+
+struct Store: Codable {
+    var id: String?
+    let name: String
+    let address: String
+}
