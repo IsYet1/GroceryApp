de49bc63104b72984108365bb92b9a74075d9a14
IsYet1 <Don@IsYet.com>
2021-11-12 17:32:35 -0600

    Add the viewmodel

--- GroceryApp/View Models/StoreListViewModel.swift
+++ GroceryApp/View Models/StoreListViewModel.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+class StoreListViewModel: ObservableObject {
+    private var firestoreManager: FirestoreManager
+    @Published var stores: [StoreViewModel] = []
+    
+    init() {
+        firestoreManager = FirestoreManager()
+    }
+    
+    func getAll() {
+        firestoreManager.getAllStores { result in
+            switch result {
+            case .success(let stores):
+                if let stores = stores {
+                    DispatchQueue.main.async {
+                        self.stores = stores.map(StoreViewModel.init)
+                    }
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+}
+
+struct StoreViewModel {
+    let store: Store
+    
+    var storeId: String {
+        store.id ?? ""
+    }
+
+    var name: String {
+        store.name
+    }
+    
+    var address: String {
+        store.address
+    }
+
+}
